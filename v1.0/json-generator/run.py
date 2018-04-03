d31d9cd12873ac537d5499a71046604c14198f20
Alejandro Barrera <alejandro.barrera@duke.edu>
2018-04-03 16:09:46 -0400

    Improve JSON generator for RNA-seq pipelines
    - All paths and constants are now configured in the consts.py file,
    - All options can be overwritten by command line arguments

--- v1.0/json-generator/run.py
+++ v1.0/json-generator/run.py
@@ -35,13 +35,29 @@ class MetadataParser(object):
         self.records = self.load_file()
         self.separate_jsons = args.separate_jsons
         self.default_adapters = args.default_adapters
+        self.genome_effective_size = args.genome_effective_size
         self.genome_ref_first_index = args.genome_ref_first_index
         self.genome_sizes_file = args.genome_sizes_file
+        self.genome_fasta_files = args.genome_fasta_files
+        self.annotation_file = args.annotation_file
+        self.rsem_rootname = args.rsem_rootname
         self.encode_blacklist_bedfile = args.encode_blacklist_bedfile
         self.trimmomatic_jar = args.trimmomatic_jar
         self.picard_jar = args.picard_jar
         self.as_narrowPeak = args.as_narrowPeak
         self.as_broadPeak = args.as_broadPeak
+        self.star_genome_dir = args.star_genome_dir
+        self.bamtools_forward_filter = args.bamtools_forward_filter
+        self.bamtools_reverse_filter = args.bamtools_reverse_filter
+        self.preserve_arguments = args.preserve_arguments
+        self.read_length = args.read_length
+
+        # To allow certain configuration files to be overriden, this is needed
+        self.purge_undef_args()
+
+    def purge_undef_args(self):
+        for empty_key in [k for k, v in self.__dict__.iteritems() if not v]:
+            del self.__dict__[empty_key]
 
     def render_json(self, wf_conf, samples_list, data_dir, template_name):
         pass
@@ -58,7 +74,10 @@ class MetadataParser(object):
         return rows
 
     def update_paths(self, ref_data_obj):
-        self.__dict__.update(ref_data_obj.__dict__)
+        options = ref_data_obj.__dict__.iteritems()
+        if self.preserve_arguments:
+            options = {(k, v) for k, v in ref_data_obj.__dict__.iteritems() if k not in self.__dict__}
+        self.__dict__.update(options)
 
 
 def generateMetadataParser(args):
@@ -96,7 +115,7 @@ class MetadataParserChipseq(object):
                 wf_key += '-with-control'
             wf_conf_dict[wf_key] = {'rt': read_type,
                                     'st': sample_info.keys()}
-            genome = 'hg38'  # Default genome
+            genome = consts.GENOME  # Default genome
             if 'Genome' in r.keys():
                 genome = r['Genome']
 
@@ -134,7 +153,6 @@ class MetadataParserAtacseq(object):
                                     'samples_list': samples_list,
                                     'data_dir': data_dir,
                                     'nthreads': self.nthreads,
-                                    'mem': self.mem,
                                     'conf_args': self
                                     })
         json_str = '\n'.join([l for l in json_str.split('\n') if l.strip() != ''])  # Remove empty lines
@@ -148,7 +166,7 @@ class MetadataParserAtacseq(object):
             sample_info = {'treatment': r['Name'],
                            'iter': r['Iter num']}
             wf_key = '-'.join([read_type])
-            genome = 'hg38'  # Default genome
+            genome = consts.GENOME  # Default genome
             if 'Genome' in r.keys():
                 genome = r['Genome']
             if not ('Blacklist removal' in r.keys() and is_false(r['Blacklist removal'])):
@@ -182,7 +200,6 @@ class MetadataParserAtacseq(object):
 class MetadataParserRnaseq(object):
     def __init__(self, **kwargs):
         self.obj = generateMetadataParser(kwargs['args_obj'])
-        self.genomeDir = kwargs['args_obj'].genomeDir
         self.skip_star_2pass = kwargs['args_obj'].skip_star_2pass
 
     def __getattr__(self, attr):
@@ -195,8 +212,7 @@ class MetadataParserRnaseq(object):
                                     'samples_list': samples_list,
                                     'data_dir': data_dir,
                                     'nthreads': self.nthreads,
-                                    'mem': self.mem,
-                                    'genomeDir': self.genomeDir
+                                    'conf_args': self
                                     })
         json_str = '\n'.join([l for l in json_str.split('\n') if l.strip() != ''])  # Remove empty lines
         return json_str
@@ -208,17 +224,33 @@ class MetadataParserRnaseq(object):
             read_type = r['Paired-end or single-end'].lower()
             sample_name = r['Name']
             strand_specific = r['Strand specificity']
+            genome = consts.GENOME  # Default genome
+            if 'Genome' in r.keys():
+                genome = r['Genome']
             kws = [read_type,  strand_specific]
             if self.skip_star_2pass:
                 kws.append('with-sjdb')
             wf_key = '-'.join(kws)
             wf_conf_dict[wf_key] = {'iter': r['Iter num'], 'rt': read_type, 'sn': sample_name}
-            samples_dict[wf_key].append(sample_name)
-        for wf_key, samples_list in samples_dict.iteritems():
+            samples_dict[wf_key].append([sample_name, genome])
+        for wf_key, samples_gemomes in samples_dict.iteritems():
             if self.obj.separate_jsons:
-                for si, s in enumerate(sorted(samples_list)):
-                    yield self.render_json(wf_conf_dict[wf_key], [s], data_dir), wf_key, si
+                for si, s in enumerate(sorted(samples_gemomes)):
+                    sample, genome = s[0], s[1]
+                    ref_dataset = consts.ReferenceDataset(genome, read_length=self.read_length)
+                    self.update_paths(ref_dataset)
+
+                    yield self.render_json(wf_conf_dict[wf_key], [sample], data_dir), wf_key, si
             else:
+                samples_list = [s[0] for s in samples_genomes]
+                genomes_list = [g[1] for g in samples_genomes]
+                if len(set(genomes_list)) > 1:
+                    raise Exception(
+                        'More than one genome specified (%s). Please create a different metadata file'
+                        ' per genome or provide a sjdb and specify the --separate-jsons argument' %
+                        ', '.join(set(genomes_list)))
+                ref_dataset = consts.ReferenceDataset(genomes_list[0], read_length=self.read_length)
+                self.update_paths(ref_dataset)
                 yield self.render_json(wf_conf_dict[wf_key], sorted(samples_list), data_dir), wf_key, None
 
 
@@ -229,6 +261,7 @@ def main():
             --------------------------------------------------------
             '''))
 
+    #############################
     # Base script options
     parser.add_argument('-o', '--outdir', metavar='output_dir', required=False, dest='outdir', type=str,
                         help='Output directory where the files will be placed.')
@@ -240,34 +273,44 @@ def main():
                         help='Project directory containing the fastq data files.')
     parser.add_argument('-t', '--metadata-type', dest='data_type', choices=['chip-seq', 'rna-seq', 'atac-seq'],
                         default='chip-seq', help='Experiment type for the metadata.')
-    parser.add_argument('--nthreads', type=int, dest='nthreads', default=16, help='Number of threads.')
-    parser.add_argument('--mem', type=int, dest='mem', default=16000, help='Memory for Java based CLT.')
+    parser.add_argument('--nthreads', type=int, dest='nthreads', default=consts.CPUS, help='Number of threads.')
+    parser.add_argument('--mem', type=int, dest='mem', default=consts.MEM, help='Memory for Java based CLT.')
     parser.add_argument('--separate-jsons', action='store_true', help='Create one JSON per sample in the metadata.')
-    parser.add_argument('--default-adapters', default='/data/reddylab/projects/GGR/auxiliary/adapters/default_adapters.fasta',
-                        help='File with adapter sequences to be trimmed out).')
-    parser.add_argument('--genome-ref-first-index', default='/data/reddylab/Reference_Data/Genomes/hg38/GCA_000001405.15_GRCh38_no_alt_analysis_set.1.ebwt',
-                        help='[non RNA-seq only] First index file of the Bowtie reference genome.')
-    parser.add_argument('--genome-sizes-file', default='/data/reddylab/projects/GGR/auxiliary/hg38.sizes',
-                        help='Chromosome sizes file')
+    parser.add_argument('--skip-star-2pass', action='store_true', default=False,
+                        help='''[RNA-seq only]
+                             Skip the STAR 2-pass step and use the genomeDir index for mapping.
+                             By default, a STAR 2-pass strategy if implemented to create a splice junctions
+                             file used to create a new STAR genome.''')
+    parser.add_argument('--preserve-arguments', action='store_true', default=False,
+                        help='''Preserve options specified in the command line over default values stored in consts.py''')
+    #############################
+    # Overwrittable default paths
+    parser.add_argument('--read-length', help='Read length of the sequenced FASTQ files.',
+                        type=int, default=consts.READ_LENGTH)
+    parser.add_argument('--default-adapters', help='File with adapter sequences to be trimmed out).')
+    parser.add_argument('--genome-ref-first-index', help='[non RNA-seq only] First index file of the Bowtie reference genome.')
+    parser.add_argument('--genome-sizes-file', help='Chromosome sizes file')
+    parser.add_argument('--encode-blacklist-bedfile', help='ENCODE blacklist bedfile to mask out un-mappable regions')
+    parser.add_argument('--genome-effective-size', help='Genome effective or mappable size. Used in some deeptools commands')
+    parser.add_argument('--star-genome-dir', help='[RNA-seq only] Directory containing the STAR Genome files (indices).')
+    parser.add_argument('--annotation-file', help='[RNA-seq only] Gene annotation GTF file')
+    parser.add_argument('--genome-fasta-files', nargs="*", help='[RNA-seq only] Genome FASTA file')
+    parser.add_argument('--rsem-rootname',
+                        help='[RNA-seq only] RSEM reference/index rootname (common path to each RSEM index file)')
     parser.add_argument('--as-narrowPeak', default=consts.as_narrowPeak,
                         help='AutoSQL file defining non-standard fields for narrowPeak files '
                              '(formats available in https://github.com/ucscGenomeBrowser/kent/tree/master/src/hg/lib/encode)')
     parser.add_argument('--as-broadPeak', default=consts.as_broadPeak,
                         help='AutoSQL file defining non-standard fields for broadPeak files '
                              '(formats available in https://github.com/ucscGenomeBrowser/kent/tree/master/src/hg/lib/encode)')
-    parser.add_argument('--encode-blacklist-bedfile', default='/data/reddylab/Reference_Data/ENCODE/hg38.blacklist.bed',
-                        help='ENCODE blacklist bedfile to mask out un-mappable regions')
-    parser.add_argument('--trimmomatic-jar', default='/data/reddylab/software/Trimmomatic-0.32/trimmomatic-0.32.jar',
+    parser.add_argument('--bamtools-forward-filter', default=consts.bamtools_forward_filter,
+                        help='Rules for forward reads used in Bamtools')
+    parser.add_argument('--bamtools-reverse-filter', default=consts.bamtools_reverse_filter,
+                        help='Rules for reverse reads used in Bamtools')
+    parser.add_argument('--trimmomatic-jar', default=consts.trimmomatic_jar,
                         help='Trimmomatic JAVA jar file')
-    parser.add_argument('--picard-jar', default='/data/reddylab/software/picard/dist/picard.jar',
+    parser.add_argument('--picard-jar', default=consts.picard_jar,
                         help='Picard JAVA jar file')
-    parser.add_argument('--genomeDir', default='/data/reddylab/Reference_Data/Genomes/hg38/STAR_genome_sjdbOverhang_49',
-                        help='[RNA-seq only] Directory containing the STAR Genome files (indices).')
-    parser.add_argument('--skip-star-2pass', action='store_true', default=False,
-                        help='''[RNA-seq only]
-                             Skip the STAR 2-pass step and use the genomeDir index for mapping.
-                             By default, a STAR 2-pass strategy if implemented to create a splice junctions
-                             file used to create a new STAR genome.''')
 
     # Parse input
     args = parser.parse_args()
